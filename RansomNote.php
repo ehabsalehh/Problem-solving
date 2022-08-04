bd2191ccd3e9508d5f5715892749a278a881a599
ehabsalehh <ehabEbrahemSaleh.com>
2022-08-04 18:51:36 +0200

    add RansomNote

---
+++ RansomNote.php
@@ -0,0 +1,30 @@
+<?php
+class RansomNote{
+    function canConstruct($ransomNote, $magazine) {
+        $magazineLength =  strlen($magazine);
+        $ransomNoteLength = strlen($ransomNote);
+        $constructArray = array_fill(0,26,0);
+        for($i =0; $i<$magazineLength; $i++){
+            $constructArray[ord($magazine[$i])- ord('a')]++;
+        }
+        for($i = 0; $i < $ransomNoteLength; $i++){
+            if(--$constructArray[ord($ransomNote[$i])- ord('a')] <0) return false;
+        }
+        return true;
+
+        // another Solution
+        // $magazineLength =  strlen($magazine);
+        // $ransomNoteLength = strlen($ransomNote);
+        // $constructArray = [];
+        // for($i =0; $i<$magazineLength; $i++){
+        //     if(!isset($constructArray[$magazine[$i]])){
+        //         $constructArray[$magazine[$i]] = 0; 
+        //     } 
+        //     $constructArray[$magazine[$i]]++;
+        // }
+        // for($i = 0; $i < $ransomNoteLength; $i++){
+        //     if($constructArray[$ransomNote[$i]]-- ==0) return false;
+        // }
+        // return true;
+    }
+}
