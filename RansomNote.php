1c66765425ad354c6658374bac0e997c3d64ade4
ehabsalehh <ehabEbrahemSaleh.com>
2022-08-31 04:51:55 +0200

    LinkedListCycle

--- RansomNote.php
+++ RansomNote.php
@@ -1,5 +1,10 @@
 <?php
 class RansomNote{
+/*
+Given two strings ransomNote and magazine,
+return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+Each letter in magazine can only be used once in ransomNote.
+*/
     function canConstruct($ransomNote, $magazine) {
         $magazineLength =  strlen($magazine);
         $ransomNoteLength = strlen($ransomNote);
